e8c7c5b965205268a7926f9d2cc1031d63e8dc2e
Maksim <sipatovgt@mail.ru>
2024-01-26 18:54:27 +0400

    complete_HW5

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS Tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titleTask VARCHAR(500) NOT NULL,
-    taskStatus VARCHAR(50) NOT NULL,
+    status VARCHAR(50) NOT NULL,
     dateTimeCreateTask TIMESTAMP NOT NULL
 );
